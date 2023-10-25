293eed29e163a3d8653c1ce68440688cf45b7098
pegasis4 <yashucan4@gmail.com>
2023-10-25 19:34:37 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='color: rgb(30 41 59);'>
+    <App />
+  </div>,
+)
